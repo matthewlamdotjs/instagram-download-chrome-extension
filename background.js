0bdf88c2779913071b191edb7dc4a25f6406f263
matthewlamdotjs <mwklam@hawaii.edu>
2019-07-19 21:04:58 -1000

    complete draft, uses confirm instead on same page to avoid CORB

--- background.js
+++ background.js
@@ -1,33 +1,54 @@
-let videos = [];
-let images = [];
+// let videos = [];
+// let images = [];
 
-function download(blob, filename) {
-    let dl = document.createElement('a');
-    dl.download = filename;
-    dl.href = blob;
-    dl.click();
-}
+// function addImage(image){
+//     images.push(image);
+// }
 
-// chrome message example modified from
-// https://stackoverflow.com/a/31112456/10817625
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        switch(message.type) {
-            case 'image':
-                images.push(message);
-                break;
-            case 'video':
-                videos.push(message);
-                break;
-            case 'clear':
-                videos = [];
-                images = [];
-                break;
-            case 'grabData':
-                sendResponse({images: images, videos: videos})
-                break;
-            default:
-                console.error("Unrecognised message: ", message);
-        }
-    }
-);
+// function addVideo(video){
+//     videos.push(video);
+// }
+
+// // Modified from:
+// // https://stackoverflow.com/questions/49474775/chrome-65-blocks-cross-origin-a-download-client-side-workaround-to-force-down
+// // Current blob size limit is around 500MB for browsers
+// function makeBlob(message, callback) {
+//     let url = message.url;
+//     let filename = message.filename;
+//     fetch(url, {
+//     headers: new Headers({
+//         'Origin': location.origin
+//     }),
+//     mode: 'cors'
+//     })
+//     .then(response => response.blob())
+//     .then(blob => {
+//     let blobUrl = window.URL.createObjectURL(blob);
+//     callback({blob: blobUrl, filename: filename});
+//     })
+//     .catch(e => console.error(e));
+// }
+
+// // chrome message example modified from
+// // https://stackoverflow.com/a/31112456/10817625
+// chrome.runtime.onMessage.addListener(
+//     function(message, sender, sendResponse) {
+//         switch(message.type) {
+//             case 'image':
+//                 images.push(message);
+//                 break;
+//             case 'video':
+//                 videos.push(message);
+//                 break;
+//             case 'clear':
+//                 videos = [];
+//                 images = [];
+//                 break;
+//             case 'grabData':
+//                 sendResponse({images: images, videos: videos})
+//                 break;
+//             default:
+//                 console.error("Unrecognised message: ", message);
+//         }
+//     }
+// );
--- posts.js
+++ posts.js
@@ -5,14 +5,24 @@ let currentUrl = '';
 setInterval(function(){
     if(window.location.href.indexOf('/p/') > -1
         && window.location.href != currentUrl){
-        downloadPosts();
         currentUrl = window.location.href;
+        downloadPosts();
     } else {
         chrome.runtime.sendMessage({type: 'clear'});
     }
 }, 500)
 
+document.addEventListener('click', function(e){
+    if(window.location.href.indexOf('/p/') > -1
+        && window.location.href != currentUrl){
+        currentUrl = window.location.href;
+        downloadPosts();
+    }
+})
+
 function downloadPosts(){
+    let videos = [];
+    let images = [];
     // detect dialog
     let firstChildren = document.body.children;
     let dialog;
@@ -27,28 +37,47 @@ function downloadPosts(){
     }
 
     function queryMedia(element){
-        let images = element.getElementsByTagName('img');
-        for(let i=0; i<images.length; i++) {
-            let image = images[i];
+        let imagesElements = element.getElementsByTagName('img');
+        let videosElements = element.getElementsByTagName('video');
+        for(let i=0; i<imagesElements.length; i++) {
+            let image = imagesElements[i];
             if(image.classList.contains('FFVAD')){
                 console.log(image.src);
-                downloadResource(image.src, 'image');
+                images.push(image.src);
             }
         }
-        let videos = element.getElementsByTagName('video');
-        for(let i=0; i<videos.length; i++) {
-            let video = videos[i];
+        for(let i=0; i<videosElements.length; i++) {
+            let video = videosElements[i];
             console.log(video.src);
-            downloadResource(video.src, 'video');
+            videos.push(video.src);
+        }
+
+        if(videos.length > 0 || images.length > 0){
+            let ans = confirm(images.length+' image(s) and '+videos.length+' video(s) found. Would you like to download them?');
+            if(ans){
+                images.forEach(function(image){
+                    downloadResource(image, 'image');
+                });
+                videos.forEach(function(video){
+                    downloadResource(video, 'video');
+                });
+            }
         }
     }
 
-    // sends media to background.js
+    // OLD: sends media to background.js
+    // UPDATE: now downloads here, use confirm on page instead. CORS workaround
     function sendToBackground(blob, filename, type) {
         filename = filename.split('\\').pop().split('/').pop()
                 .split(type == 'image' ? '.jpg' : '.mp4')[0]
                 +(type == 'image' ? '.jpg' : '.mp4');
-        chrome.runtime.sendMessage({type: type, filename: filename, blob: blob});
+        // chrome.runtime.sendMessage({type: type, filename: filename, blob: blob});
+
+        // Download here instead after confirm
+        let dl = document.createElement('a');
+        dl.download = filename;
+        dl.href = blob;
+        dl.click();
     }
 
     // Modified from:
